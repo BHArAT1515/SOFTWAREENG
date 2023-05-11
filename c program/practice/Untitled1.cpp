e317042f41f09d9cdfefaf2481b95702250319c9
BharatShing <singhbharat@myyahoo.com>
2023-05-11 09:18:06 +0530

    first commit

---
+++ c program/practice/Untitled1.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main(){
+	printf("\t        ********************");
+	printf("\n \t        * \t");
+	printf("\n*\t        * \t");
+	printf("\n*\t        * \t");
+	printf("\n*\t        * \t");
+	printf("\n*\t        * \t");
+	printf("\n*\t        * \t");
+	printf("\n*\t        * \t");
+	printf("\n ********************************");
+	printf("\n\t        *\t\t*");
+	printf("\n\t        *\t\t*");
+	printf("\n\t        *\t\t*");
+	printf("\n\t        *\t\t*");
+	printf("\n\t        *\t\t*");
+	printf("\n\t        *\t\t*");
+	printf("\n\t        *\t\t*");
+	printf("\n*****************");
+}
